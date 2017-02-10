49e66ac290ab23795fa0319e407a0a3d81c35703
d3fk <Angatar@users.noreply.github.com>
2017-02-10 14:57:18 +0100

    Update entry script to run rpc.statd
    rpc.statd is essential for using this nfs-client in association with a database (mariadb, mysql...)

--- entry.sh
+++ entry.sh
@@ -3,6 +3,9 @@
 set -m
 
 mkdir -p "$MOUNTPOINT"
+
+rpc.statd
+
 if [$SERVER == ""]; then
 rpcbind -f & echo "docker NFS client with rpcbind ENABLED... if you wish to mount the mountpoint in this container USE THE FOLLOWING SYNTAX instead:
 $ docker run -itd --privileged=true --net=host -v vol:/mnt/nfs-1:shared -e SERVER= X.X.X.X -e SHARE=shared_path d3fk/nfs-client" & more
